c935cdb0f340f1e5d2747cc4ba508d0fe79ef772
andres velasco <velasco.andrs@gmail.com>
2020-04-20 22:05:16 -0500

    para el tema del sistema

---
+++ enums/colores.dart
@@ -0,0 +1,5 @@
+enum TemaSistema {
+  azul,
+  rojo,
+  oscuro,
+}
--- repositorios/almacenamiento_local_repositorio.dart
+++ repositorios/almacenamiento_local_repositorio.dart
@@ -1,22 +1,70 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:open_market_movil/src/submodulos/submodulo-auth0-movil/enums/colores.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AlmacenamientoLocalRepositorio {
   Locale _lenguajeAplicacion = Locale('en');
+  MaterialColor _temaAplicacion = Colors.blue;
 
+  // getters
   Locale get lenguajeAplicacion => _lenguajeAplicacion ?? Locale("en");
 
-  Future<void> recuperarLenguajeDelSistema() async{
-    SharedPreferences almacenamientoLocal = await SharedPreferences.getInstance();
+  MaterialColor get temaAplicacion => _temaAplicacion ?? Colors.blue;
+
+  // metodos
+
+  // cargar el lenguaje del sistema desde le almacenamiento local
+  Future<void> _recuperarLenguajeDelSistema() async {
+    SharedPreferences almacenamientoLocal =
+        await SharedPreferences.getInstance();
     String lenguaje = almacenamientoLocal.getString('lang_code');
-    bool tieneGuardadoLenguaje =  lenguaje != null;
+    bool tieneGuardadoLenguaje = lenguaje != null;
     if (tieneGuardadoLenguaje) {
       _lenguajeAplicacion = Locale(lenguaje);
     } else {
       _lenguajeAplicacion = Locale('en');
     }
     return;
+  }
+
+  Future<void> obtenerInformacionGuardada() async{
+    await this._recuperarLenguajeDelSistema();
+    await this._recuperarTemaDelSistema();
+  }
+
+  Future<void> _recuperarTemaDelSistema() async {
+    SharedPreferences almacenamientoLocal =
+        await SharedPreferences.getInstance();
+    String tema = almacenamientoLocal.getString('tema');
+    switch (tema) {
+      case 'azul':
+        _temaAplicacion = Colors.blue;
+        break;
+      case 'oscuro':
+        _temaAplicacion = Colors.black38;
+        break;
+      case 'rojo':
+        _temaAplicacion = Colors.red;
+        break;
+      default:
+        _temaAplicacion = Colors.blue;
+        break;
+    }
+  }
+
+  Future<void> guardarTema(TemaSistema tema) async {
+    var almacenamientoLocal = await SharedPreferences.getInstance();
+    if (tema == TemaSistema.azul) {
+      await almacenamientoLocal.setString('tema', 'azul');
+    }
+    if (tema == TemaSistema.rojo) {
+      await almacenamientoLocal.setString('tema', 'rojo');
+
+    }
+    if (tema == TemaSistema.oscuro) {
+      await almacenamientoLocal.setString('tema', 'oscuro');
+    }
   }
 
   Future<void> cambiarIdioma(Locale lenguaje) async {
